b59c3397c615f240ef3d42747099870701014cbc
Anduina <edsonjuniortdeveloper@gmail.com>
2015-02-04 18:09:47 -0200

    routers do active

--- js/router.js
+++ js/router.js
@@ -1,5 +1,7 @@
 Todos.Router.map(function(){
-    this.resource('todos', { path : '/'},function(){});
+    this.resource('todos', { path : '/'},function(){
+    	this.route('active');
+    });
 });
 
 Todos.TodosRoute = Ember.Route.extend({
@@ -11,4 +13,14 @@ Todos.TodosIndexRoute = Ember.Route.extend({
 	model: function(){
 		return this.modelFor('todos');
 	}
+});
+Todos.TodsoActiveRoute = Ember.Route.extend({
+	model: function(){
+		return this.store.filter('todo', function(todo){
+			return !todo.get('isCompleted');
+		});
+	}
+   ,renderTemplate: function(controller){
+   		this.render('todo/index',{controller: controller});
+   }
 });
