0d3de3362087f19040f7ccf9e0b38e71e5c38d1d
Anduina <edsonjuniortdeveloper@gmail.com>
2015-02-05 23:11:53 -0200

    Finish

--- js/controllers/todos_controller.js
+++ js/controllers/todos_controller.js
@@ -1,7 +1,12 @@
 
 Todos.TodosController = Ember.ArrayController.extend({
     actions: {
-        createTodo: function() {
+        clearComplemet: function(){
+            var completed = this.filterBy('isCompleted', true);
+            completed.invoke('deleteRecord');
+            completed.invoke('save');
+        }
+       ,createTodo: function() {
             var title = this.get('newTitle');
             if (!title.trim()) {
                 return;
@@ -15,12 +20,27 @@ Todos.TodosController = Ember.ArrayController.extend({
 
             todo.save();
         }
-    },
-    remaining: function() {
+    }
+   ,hasCompleted: function(){
+        return this.get('completed') > 0;
+    }.property('completed')
+   ,completed: function(){
+        return this.filterBy('isCompleted', true).get('length');
+   }.property('@each.isCompleted')
+   ,remaining: function() {
         return this.filterBy('isCompleted', false).get('length')
-    }.property('@each.isCompleted'),
-    inflection: function() {
+    }.property('@each.isCompleted')
+   ,inflection: function() {
         var remaining = this.get('remaining');
         return remaining <= 1 ? 'item' : 'items';
     }.property('remaining')
+   ,allAreDone: function(key, value){
+        if(value === undefined){
+            return !!this.get('length') && this.isEvery('isCompleted', true);
+        }else{
+            this.setEach('isCompleted', value);
+            this.invoke('save');
+            return value;
+        }
+    }.property('@each.isCompleted')
 });
